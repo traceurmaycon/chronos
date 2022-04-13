cf73bca037fc0c95d001decaf672b2dd97eba9d6
traceurmaycon <lopesmaycon13@gmail.com>
2022-04-13 12:49:01 -0300

    Commit inicial

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+export default function Home() {
+	return (
+		<>
+			<div className="bg-gradient-to-b from-zinc-900 to-slate-800">
+				<div className="container mx-auto min-h-screen text-center flex items-center justify-center flex-col gap-5  text-slate-300">
+					<h1 className="text-4xl tracking-[40px] after:ml-[-40px] font-share">
+						CHRONOS
+					</h1>
+					<h2 className="text-2xl">
+						Seu sistema de gestão e controle de ponto
+					</h2>
+				</div>
+			</div>
+		</>
+	);
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx}",
+		"./src/components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {},
+		fontFamily: {
+			share: ["Share", "Verdana", "sans-serif"],
+			roboto: ["Roboto", "Arial", "sans-serif"],
+		},
+	},
+	plugins: [],
+};
